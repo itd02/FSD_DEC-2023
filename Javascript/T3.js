08ab07f99eb199f6fbcdd0b086efb6f784e683e0
itd <itdefine01@gmail.com>
2024-01-16 10:25:57 +0530

    updated

---
+++ Javascript/T3.js
@@ -0,0 +1,50 @@
+/*
+		This Tutorial is About Numbers and Strings
+*/
+
+
+// let height = 5.7
+// const weight
+
+// console.log(typeof(height))
+
+// console.log(typeof(weight))
+
+
+// height = 6
+// weight  = "asdsad"
+
+// console.log(typeof(height))
+
+// console.log(typeof(weight))
+
+
+//let information = "Hi I am Virat and am a \"Cricketer\" "
+
+// let information = `Contrary to popular belief, Lorem Ipsum is not 
+// simply random text. It has roots in a piece of classical Latin
+//  literature from 45 BC, making it over 2000 years old. Richard
+//  McClintock, a Latin professor at Hampden-Sydney College in Virginia,
+//  looked up one of the more obscure Latin words, consectetur, from a
+//  Lorem Ipsum passage, and going through the cites of the word in
+//  classical literature, discovered the undoubtable source. Lorem Ipsum
+//  comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" 
+//  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a 
+//  treatise on the theory of ethics, very popular during the Renaissance. The 
+//  first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
+//  in section 1.10.32.`
+
+
+// console.log(information)
+// console.log(typeof(information))
+
+let information = "Hi I am Virat and am a Cricketer "
+
+//let last_index = information.length-1
+
+console.log(information.length)
+
+console.log('The character \\ present at last position is ')
+
+
+
