bee9d87441702679d12b2f74cfc37f6d6460958b
itd <itdefine01@gmail.com>
2024-01-17 10:22:33 +0530

    updated for jan 17 tutorial

---
+++ Test/t4.js
@@ -0,0 +1,18 @@
+
+
+
+let country = 'Iran'
+
+let data = `country launched attacks Tuesday 
+in Pakistan targeting what it described 
+as bases for the militant group Jaish 
+al-Adl, potentially further raising 
+tensions in a Middle East already roiled 
+by Israel’s war on Hamas in the Gaza Strip. 
+Pakistan said the strikes killed two children
+and wounded three others in an assault it 
+described as an “unprovoked violation” of 
+its airspace.`
+
+
+console.log(data)
