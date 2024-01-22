0e034103a3817c9ee00d179702852ef9577955b7
itd <itdefine01@gmail.com>
2024-01-22 10:03:45 +0530

    updated

---
+++ Javascript/T8.js
@@ -0,0 +1,20 @@
+// Conditionals
+
+/*
+	if
+	if, else
+	if, else if 
+	if, else if, else
+	switch
+
+	ternary operator
+*/
+
+
+let x = 0
+
+x ? console.log('X has Truthy Value')
+  : console.log('X has Falsy Value') 
+
+
+if (true) console.log("Ok")
